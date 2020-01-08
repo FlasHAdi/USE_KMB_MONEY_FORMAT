f30fb7fc08b9324fdb92480bd2f5921fe43b8bf0
Owsap <owsap@mail.com>
2020-01-08 19:43:03 -0100

    Added upper case letter

--- Client/root/constInfo.py
+++ Client/root/constInfo.py
@@ -10,5 +10,8 @@ if USE_MONEY_K_FORMAT:
 		if 'k' in moneyString:
 			if len(moneyString) > 1:
 				money = int(moneyString.replace('k', '000'))
+		elif 'K' in moneyString:
+			if len(moneyString) > 1:
+				money = int(moneyString.replace('K', '000'))
 
 		return money
