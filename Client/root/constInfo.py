f60101eb36c35972cc05ae47f5500ca4a0cf13ab
Owsap <owsap@mail.com>
2020-02-18 01:59:07 -0100

    Update constInfo.py
    Added Vega's collaboration.

--- Client/root/constInfo.py
+++ Client/root/constInfo.py
@@ -3,14 +3,29 @@
 USE_MONEY_K_FORMAT = True
 
 if USE_MONEY_K_FORMAT:
-	def FormatMoneyToK(string):
-		moneyString = str(string)
-		money = 0
+	def __ConvertMoneyText(self, text, powers = dict(k = 10**3, m = 10**6, b = 10**9)):
+		"""
+		Format string value in thousands, millions or billions.
 
-		if len(moneyString) > 1:
-			if 'k' in moneyString:
-				money = int(moneyString.replace('k', '000'))
-			elif 'K' in moneyString:
-				money = int(moneyString.replace('K', '000'))
+		'1k' = 1.000
+		'100kk' = 100.000.000
+		'100m' = 100.000.000
+		'1b' = 1.000.000.000
+		'1kmb' = 1.000 (can't use multiple suffixes types)
 
-		return money
+		:param text: string
+		:return: int
+		:date: 10.01.2020
+		:author: Vegas
+		"""
+
+		match = re.search(r'(\d+)({:s}+)?'.format('+|'.join(powers.keys())), text, re.I)
+		if match:
+			moneyValue, suffixName = match.groups()
+			moneyValue = int(moneyValue)
+			if not suffixName:
+				return moneyValue
+
+			return moneyValue * (powers[suffixName[0]] ** len(suffixName))
+
+		return 0
